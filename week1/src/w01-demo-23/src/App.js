38c6ed4ec1080d7d52c563541e64aaaa352b1197
Mario Catuogno <lister.dollar-0c@icloud.com>
2024-09-21 20:07:05 +0800

    W01-P2: Give your name and ID in demo using create-react-app and vite

--- week1/src/w01-demo-23/src/App.js
+++ week1/src/w01-demo-23/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Kevin Yu 912410023
         </a>
       </header>
     </div>
