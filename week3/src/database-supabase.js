19bbb6f9596f3927bf38d19dd31a2fafd03586cd
Mario Catuogno <lister.dollar-0c@icloud.com>
2024-10-13 21:11:42 +0800

    W03-P5: Using route /api/blog_xx to get json data from Supabase, and show it in the browser

---
+++ week3/src/database-supabase.js
@@ -0,0 +1,26 @@
+import pg from 'pg'
+const { Pool } = pg
+
+const pool = new Pool({
+    host: 'aws-0-ap-southeast-1.pooler.supabase.com',
+    port:'5432',
+    user: 'postgres.yvjzzqlmjoisjuzjhffw',
+    password: 'SPXE8l8bvbNl5oyi',
+    database: 'postgres',
+})
+
+console.log('connecting Supabase database', pool.options.database)
+
+// const testDB = async () => {
+//     try{
+//         const results = await pool.query(`select * from blog_49`)
+//         console.log('json data', results.rows)
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+
+// testDB()
+
+export default pool
+
--- week3/src/server.js
+++ week3/src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import  pool from '../src/db.js';
+import  supa from '../src/database-supabase.js';
 
 
 const app = express();
@@ -8,6 +9,16 @@ const port = process.env.port|| 5000;
 app.get('/api/blogs_23', async (req, res) => {
   try {
       const result = await pool.query('SELECT * FROM blog_post_23');
+      console.log("json data: ",result.rows);
+      res.json(result.rows);
+  }   catch (error) {
+      console.log(error);
+  }
+});
+
+app.get('/api/supa/blogs_23', async (req, res) => {
+  try {
+      const result = await supa.query('SELECT * FROM blog_post_23');
       console.log("json data: ",result.rows);
       res.json(result.rows);
   }   catch (error) {
