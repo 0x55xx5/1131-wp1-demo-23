e440b6ab0f29457884134bcdb3acd2aae8446d66
Mario Catuogno <lister.dollar-0c@icloud.com>
2024-11-20 20:39:00 +0800

    W11-P3: login logout

--- w11/src/w11_advanced_hooks_xx/src/App.jsx
+++ w11/src/w11_advanced_hooks_xx/src/App.jsx
@@ -1,12 +1,15 @@
 import T_sample_code_ from './tutorial/03-conditional-rendering/P3_02-multiple-returns-fetch-data'
 import T_sample_code_6 from './tutorial/03-conditional-rendering/P3_06-toggle-challenge'
 
+import T_sample_code_7 from './tutorial/03-conditional-rendering/P3_07-user-challenge'
+
 function App() {
   return (
     <div className='container'>
       <h2>Advanced React KKK Skills</h2>
       <T_sample_code_ />
       <T_sample_code_6 />
+      <T_sample_code_7 />
     </div>
         
   );
--- w11/src/w11_advanced_hooks_xx/src/tutorial/03-conditional-rendering/P3_07-user-challenge.jsx
+++ w11/src/w11_advanced_hooks_xx/src/tutorial/03-conditional-rendering/P3_07-user-challenge.jsx
@@ -1,5 +1,51 @@
+import { useEffect, useState } from "react";
 const UserChallenge = () => {
-  return <h2>user challenge</h2>;
+  const [user, setUser] = useState(null);
+  const login = () => {
+    setUser({ name: " 912410023 kkkkk" });
+  };
+  const logout = () => {
+    setUser(null);
+  };
+
+  return (
+    <>
+      {user ? (
+        <div>
+          <h3>Hello ,{user.name}</h3>
+          <button
+            className="btn"
+            onClick={() => {
+              logout();
+            }}
+          >
+            Logout
+          </button>
+        </div>
+      ) : (
+        <div>
+          {" "}
+          <h3>Error ,login first</h3>
+          <button
+            className="btn"
+            onClick={() => {
+              login();
+            }}
+          >
+            Login
+          </button>
+          <button
+            className="btn"
+            onClick={() => {
+              logout();
+            }}
+          >
+            Logout
+          </button>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default UserChallenge;
